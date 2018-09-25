96e691e537c2770408dbfa2ce882390078078af5
Daniele Scasciafratte <mte90net@gmail.com>
2018-09-25 15:21:11 +0200

    support for all the folders

--- generator/remove.php
+++ generator/remove.php
@@ -34,39 +34,63 @@ function remove_file( $file ) {
     $return = false;
 
     switch ( $file ) {
-        case strpos( $file, '_ActDeact.php' ) && $config[ 'act-deact_actdeact' ] === 'false':
-        case strpos( $file, '_ImpExp.php' ) && $config[ 'backend_impexp-settings' ] === 'false':
-        case strpos( $file, '_Uninstall.php' ) && $config[ 'act-deact_uninstall' ] === 'false':
-        case strpos( $file, '_FakePage.php' ) && $config[ 'libraries_wpbp__fakepage' ] === 'false':
-        case strpos( $file, '_Pointers.php' ) && $config[ 'libraries_wpbp__pointerplus' ] === 'false':
-        case strpos( $file, '_template.php' ) && $config[ 'libraries_wpbp__template' ] === 'false':
-        case strpos( $file, '_CMB.php' ) && $config[ 'libraries_webdevstudios__cmb2' ] === 'false':
-        case strpos( $file, '_ContextualHelp.php' ) && $config[ 'libraries_kevinlangleyjr__wp-contextual-help' ] === 'false':
-        case strpos( $file, '_PostTypes.php' ) && $config[ 'libraries_johnbillion__extended-cpts' ] === 'false':
-        case strpos( $file, '/help-docs' ) && $config[ 'libraries_kevinlangleyjr__wp-contextual-help' ] === 'false':
-        case strpos( $file, '/templates' ) && $config[ 'frontend_template-system' ] === 'false':
-        case strpos( $file, '/widgets' ) && $config[ 'libraries_wpbp__widgets-helper' ] === 'false':
-        case strpos( $file, '/public/assets/js' ) && $config[ 'public-assets_js' ] === 'false':
-        case strpos( $file, '/public/assets/css' ) && $config[ 'public-assets_css' ] === 'false':
-        case strpos( $file, '/public/assets/sass' ) && $config[ 'public-assets_css' ] === 'false':
-        case strpos( $file, '/public/assets' ) && $config[ 'public-assets_css' ] === 'false' && $config[ 'public-assets_js' ] === 'false':
-        case strpos( $file, '/public/ajax' ) && $config[ 'ajax_public' ] === 'false':
-        case strpos( $file, '/admin/ajax' ) && $config[ 'ajax_admin' ] === 'false':
+        // Admin folder
+        case strpos( $file, 'actdeact.php' ) && $config[ 'act-deact_actdeact' ] === 'false':
+        case strpos( $file, 'admin-enqueue.php' ) && $config[ 'admin-assets_js' ] === 'false' 
+                && $config[ 'admin-assets_css' ] === 'false':
+        case strpos( $file, 'impexp.php' ) && $config[ 'backend_impexp-settings' ] === 'false':
+        case strpos( $file, 'uninstall.php' ) && $config[ 'act-deact_uninstall' ] === 'false':
+        case strpos( $file, 'notices.php' ) && $config[ 'libraries_nathanielks__wp-admin-notice' ] === 'false' 
+                && $config[ 'libraries_julien731__wp-review-me' ] === 'false' 
+                && $config[ 'libraries_julien731__wp-dismissible-notices-handler' ] === 'false':
+        case strpos( $file, 'settings-page.php' ) && $config[ 'admin-assets_admin-page' ] === 'false':
         case strpos( $file, '/admin/views' ) && $config[ 'admin-assets_admin-page' ] === 'false':
-        case strpos( $file, '/admin/assets' ) && $config[ 'admin-assets_admin-page' ] === 'false':
-        case strpos( $file, '/admin/assets/css/admin' ) && $config[ 'admin-assets_admin-css' ] === 'false':
-        case strpos( $file, '/admin/assets/sass/admin' ) && $config[ 'admin-assets_admin-css' ] === 'false':
-        case strpos( $file, '/admin/assets/js/admin' ) && $config[ 'admin-assets_admin-js' ] === 'false':
-        case strpos( $file, '/admin/assets/js/settings' ) && $config[ 'admin-assets_settings-js' ] === 'false':
-        case strpos( $file, '/admin/assets/coffee/admin' ) && $config[ 'admin-assets_admin-js' ] === 'false':
-        case strpos( $file, '/admin/assets/coffee/settings' ) && $config[ 'admin-assets_settings-js' ] === 'false':
-        case strpos( $file, '/admin/assets/css/settings' ) && $config[ 'admin-assets_settings-css' ] === 'false':
-        case strpos( $file, '/admin/assets/sass/settings' ) && $config[ 'admin-assets_settings-css' ] === 'false':
+        // Ajax folder
+        case strpos( $file, 'class-ajax.php' ) && $config[ 'ajax_public' ] === 'false':
+        case strpos( $file, 'admin-ajax.php' ) && $config[ 'ajax_admin' ] === 'false':
+        // Assets folder
+        case strpos( $file, 'public.js' ) && $config[ 'public-assets_js' ] === 'false':
+        case strpos( $file, 'public.css' ) && $config[ 'public-assets_css' ] === 'false':
+        case strpos( $file, 'public.sass' ) && $config[ 'public-assets_css' ] === 'false':
+        case strpos( $file, 'admin.css' ) && $config[ 'admin-assets_admin-css' ] === 'false':
+        case strpos( $file, 'admin.sass' ) && $config[ 'admin-assets_admin-css' ] === 'false':
+        case strpos( $file, 'admin.coffee' ) && $config[ 'admin-assets_admin-js' ] === 'false':
+        case strpos( $file, 'admin.js' ) && $config[ 'admin-assets_admin-js' ] === 'false':
+        case strpos( $file, 'settings.js' ) && $config[ 'admin-assets_settings-js' ] === 'false':
+        case strpos( $file, 'settings.coffee' ) && $config[ 'admin-assets_settings-js' ] === 'false':
+        case strpos( $file, 'settings.css' ) && $config[ 'admin-assets_settings-css' ] === 'false':
+        case strpos( $file, 'settings.sass' ) && $config[ 'admin-assets_settings-css' ] === 'false':
+        // Cli folder
+        case strpos( $file, 'cli.php' ) && $config[ 'wpcli' ] === 'false':
+        // Integrations
+        case strpos( $file, 'cmb.php' ) && $config[ 'libraries_webdevstudios__cmb2' ] === 'false':
+        case strpos( $file, 'contextualhelp.php' ) && $config[ 'libraries_mte90__wp-contextual-help' ] === 'false':
+        case strpos( $file, '/help-docs' ) && $config[ 'libraries_mte90__wp-contextual-help' ] === 'false':
+        case strpos( $file, 'cron.php' ) && $config[ 'libraries_wpbp__cronplus' ] === 'false':
+        case strpos( $file, 'fakepage.php' ) && $config[ 'libraries_wpbp__fakepage' ] === 'false':
+        case strpos( $file, 'pointers.php' ) && $config[ 'libraries_wpbp__pointerplus' ] === 'false':
+        case strpos( $file, 'template.php' ) && $config[ 'libraries_wpbp__template' ] === 'false':
+        case strpos( $file, '/widgets' ) && $config[ 'libraries_wpbp__widgets-helper' ] === 'false':
+        // Internals
+        case strpos( $file, 'posttypes.php' ) && $config[ 'libraries_johnbillion__extended-cpts' ] === 'false':
+        case strpos( $file, 'shortcode.php' ) && $config[ 'frontend_shortcode' ] === 'false':
+        case strpos( $file, 'transient.php' ) && $config[ 'system_transient' ] === 'false':
+        case strpos( $file, 'debug.php' ) && $config[ 'libraries_wpbp__debug' ] === 'false':
+        // Public
+        case strpos( $file, 'class-enqueue.php' ) && $config[ 'public-assets_js' ] === 'false' 
+                && $config[ 'public-assets_css' ] === 'false'
+                && $config[ 'frontend_wp-localize-script' ]:
+        // REST folder
+        case strpos( $file, 'rest.php' ) && $config[ 'system_rest' ] === 'false':
+        // Template folder
+        case strpos( $file, '/templates' ) && $config[ 'frontend_template-system' ] === 'false':
+        // Tests folder
         case strpos( $file, '/tests' ) && $config[ 'unit-test' ] === 'false':
-        case strpos( $file, 'codeception.yml' ) && $config[ 'unit-test' ] === 'false':
+        case strpos( $file, 'codeception.dist.yml' ) && $config[ 'unit-test' ] === 'false':
+        case strpos( $file, '.env' ) && $config[ 'unit-test' ] === 'false':
         case strpos( $file, 'wp-config-test.php' ) && $config[ 'unit-test' ] === 'false':
+        // Others
         case strpos( $file, 'languages' ) && $config[ 'language-files' ] === 'false':
-        case strpos( $file, '_WPCli.php' ) && $config[ 'wpcli' ] === 'false':
         case strpos( $file, 'grumphp.yml' ) && $config[ 'grumphp' ] === 'false':
             $return = remove_file_folder( $file );
             break;
