022cfbc6731d8991de9f0bc5bed83c95682d6937
Daniele Scasciafratte <mte90net@gmail.com>
2019-02-22 13:02:19 +0100

    bump

--- generator/init.php
+++ generator/init.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WPBP_VERSION', '3.0' );
+define( 'WPBP_VERSION', '3.0.1' );
 require_once(dirname( __FILE__ ) . '/miscellaneous.php');
 require_once(dirname( __FILE__ ) . '/composer.php');
 require_once(dirname( __FILE__ ) . '/tools.php');
